6601f1b6dbf0470457809182c3a4829ca9a00aab
Ahmed <79737161+Ahmed-0102@users.noreply.github.com>
2023-03-04 14:45:58 +0530

    Side drawer on click function.

--- app/src/main/java/com/example/googlekeep/MainActivity.java
+++ app/src/main/java/com/example/googlekeep/MainActivity.java
@@ -1,187 +1,97 @@
 package com.example.googlekeep;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity implements NotesListener {
+public class MainActivity extends AppCompatActivity {
 
-    DrawerLayout drawerLayout;
+    DrawerLayout drawer_layout;
     NavigationView navigationView;
+
+    FrameLayout frame_layout;
+
     ActionBarDrawerToggle drawerToggle;
+
     Toolbar toolbar;
-    private RecyclerView notesRecycler;
-    private NoteAdapter noteAdapter;
-    private FloatingActionButton createNoteFab;
-    private EditText search;
-    ArrayList<Notes> notesArrayList = new ArrayList<>();
-    ImageView profile;
 
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = findViewById(R.id.toolbar);
-        drawerLayout = findViewById(R.id.drawer_layout);
+        frame_layout = findViewById(R.id.frame_layout);
+        drawer_layout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
-        notesRecycler = findViewById(R.id.notes_recycler);
-        createNoteFab = findViewById(R.id.fab_btn);
-        search = findViewById(R.id.search);
-        profile = findViewById(R.id.profile);
-
-        profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(MainActivity.this, SignIn.class));
-                finish();
-            }
-        });
-
-        // for removing keyboard
-        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-
+        toolbar = findViewById(R.id.toolbar);
+        drawerToggle = new ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.open, R.string.close);
+        drawer_layout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
 
         // Setting Custom Action Bar
         setSupportActionBar(toolbar);
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.notes: {
-                        Toast.makeText(MainActivity.this, "Notes Activity", Toast.LENGTH_SHORT).show();
-                        break;
-                    }
-                    case R.id.archive: {
-                        Toast.makeText(MainActivity.this, "Archive Activity", Toast.LENGTH_SHORT).show();
-                        break;
-                    }
-                    case R.id.delete: {
-                        Toast.makeText(MainActivity.this, "Delete Activity", Toast.LENGTH_SHORT).show();
-                        break;
-                    }
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.archive_icon);
+
+
+
+
+        replaceFragment(new NotesFragment());
+
+        if (savedInstanceState == null) {
+            navigationView.setCheckedItem(R.id.notes);
+        }
+
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.notes: {
+                    replaceFragment(new NotesFragment());
+                    Toast.makeText(this, "Notes Activity", Toast.LENGTH_SHORT).show();
+                    break;
                 }
-                return true;
+                case R.id.archive: {
+                    replaceFragment(new ArchiveFragment());
+                    Toast.makeText(this, "Archive Activity", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                case R.id.delete: {
+                    Toast.makeText(this, "Delete Activity", Toast.LENGTH_SHORT).show();
+                    break;
+                }
             }
-        });
-        drawerToggle.syncState();
-        drawerLayout.addDrawerListener(drawerToggle);
-
-        createNoteFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent createNoteIntent = new Intent(MainActivity.this, CreateNote.class);
-                startActivity(createNoteIntent);
-            }
+            drawer_layout.closeDrawer(GravityCompat.START);
+            return true;
         });
 
-        // Recycler View
-        setNotesRecycler();
-
-
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        setData();
-    }
-
-    private void setData() {
-        notesArrayList.clear();
-        Utility.getCollectionReferenceForNotes().get()
-                .addOnSuccessListener(queryDocumentSnapshots -> {
-                    if (queryDocumentSnapshots.isEmpty()) {
-                        return;
-                    } else {
-                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
-                            Notes note = documentSnapshot.toObject(Notes.class);
-                            note.setDocumentId(documentSnapshot.getId());
-                            notesArrayList.add(note);
-                        }
-                        noteAdapter.notifyDataSetChanged();
-                    }
-                })
-                .addOnFailureListener(e -> {
-                    Toast.makeText(this, "Error in getting data", Toast.LENGTH_SHORT).show();
-                });
-    }
-
-    private void setNotesRecycler() {
-        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
-        notesRecycler.setLayoutManager(staggeredGridLayoutManager);
-        noteAdapter = new NoteAdapter(this, notesArrayList, this);
-        notesRecycler.setAdapter(noteAdapter);
     }
 
     @Override
     public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
     }
 
-    @Override
-    public void onNoteClicked(Notes notes, int position) {
-        Intent intent = new Intent(MainActivity.this, CreateNote.class);
-        intent.putExtra("isEdited", true);
-        intent.putExtra("DocId", notes.getDocumentId());
-        intent.putExtra("title", notes.getNoteTitle());
-        intent.putExtra("desc", notes.getNoteDesc());
-        startActivity(intent);
-    }
-
-
-    @Override
-    public void onLongClickMenu(boolean isSelected) {
-        if (isSelected){
-            getSupportActionBar().hide();
-        }else {
-            getSupportActionBar().show();
-        }
-    }
 
 //    @Override
 //    public void onLongClick() {
@@ -197,4 +107,19 @@ public class MainActivity extends AppCompatActivity implements NotesListener {
 //            isSelected = false;
 //        }
 //    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
 }
---
+++ app/src/main/java/com/example/googlekeep/NotesFragment.java
@@ -0,0 +1,128 @@
+package com.example.googlekeep;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class NotesFragment extends Fragment implements NotesListener{
+    private RecyclerView notesRecycler;
+    private NoteAdapter noteAdapter;
+    private FloatingActionButton createNoteFab;
+    private EditText search;
+    ArrayList<Notes> notesArrayList = new ArrayList<>();
+    ImageView profile;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_notes, container, false);
+        notesRecycler = view.findViewById(R.id.notes_recycler);
+        createNoteFab = view.findViewById(R.id.fab_btn);
+        search = view.findViewById(R.id.search);
+        profile = view.findViewById(R.id.profile);
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(requireActivity(), SignIn.class));
+                requireActivity().finish();
+            }
+        });
+
+        // for removing keyboard
+        requireActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        createNoteFab.setOnClickListener(view1 -> {
+            Intent createNoteIntent = new Intent(requireActivity(), CreateNote.class);
+            startActivity(createNoteIntent);
+        });
+
+        // Recycler View
+        setNotesRecycler();
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        setData();
+    }
+    private void setData() {
+        notesArrayList.clear();
+        Utility.getCollectionReferenceForNotes().get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    if (queryDocumentSnapshots.isEmpty()) {
+                        return;
+                    } else {
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                            Notes note = documentSnapshot.toObject(Notes.class);
+                            note.setDocumentId(documentSnapshot.getId());
+                            notesArrayList.add(note);
+                        }
+                        noteAdapter.notifyDataSetChanged();
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(requireActivity(), "Error in getting data", Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    private void setNotesRecycler() {
+        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        notesRecycler.setLayoutManager(staggeredGridLayoutManager);
+        noteAdapter = new NoteAdapter(requireActivity(), notesArrayList, this);
+        notesRecycler.setAdapter(noteAdapter);
+    }
+
+
+
+    @Override
+    public void onNoteClicked(Notes notes, int position) {
+        Intent intent = new Intent(requireActivity(), CreateNote.class);
+        intent.putExtra("isEdited", true);
+        intent.putExtra("DocId", notes.getDocumentId());
+        intent.putExtra("title", notes.getNoteTitle());
+        intent.putExtra("desc", notes.getNoteDesc());
+        startActivity(intent);
+    }
+
+    @Override
+    public void onLongClickMenu(boolean isSelected) {
+        if (isSelected){
+            ((AppCompatActivity) requireActivity()).getSupportActionBar().hide();
+        }else {
+            ((AppCompatActivity) requireActivity()).getSupportActionBar().show();
+        }
+    }
+
+
+}
